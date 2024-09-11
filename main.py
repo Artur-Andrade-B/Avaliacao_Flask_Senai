aca74dffe6374c502272253a91178e57e434fb15
Artur-Andrade-B <artur.and.bat@gmail.com>
2024-09-11 09:44:00 -0300

    conecção com o banco de dados realizada e placeholder html

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 from flask import Flask, render_template
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.automap import automap_base
+
 import hashlib
 import getpass
+
+from cliente import Cliente
+from funcionario import Funcionario
+from agendamento import Agendamento
+from servico import Servico
+
+from datetime import datetime, timezone
+
+import urllib.parse
+
+
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+user = "root"
+password = urllib.parse.quote_plus("senai@123")
+host = "localhost"
+database = "rei_app"
+
+connection_string = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+
+engine = create_engine(connection_string)
+metadata = MetaData()
+metadata.reflect(engine)
+
+base = automap_base(metadata=metadata)
+base.prepare()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Cliente = base.classes.cliente
+Funcionario = base.classes.funcionario
+Agendamento = base.classes.agendamento
+Servico = base.classes.servico
+
 
 app = Flask(__name__)
 
